11cde4e75c6f5a5167e58df25386b5f93cc33968
Leonardo Mouzart <leonardomouzart@gmail.com>
2026-05-19 12:35:31 -0300

    ICriação da página e estrutura do projeto

---
+++ app.py
@@ -0,0 +1,17 @@
+# ------------------------ Importando as bibliotecas ---------------------
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+
+
+
+st.set_page_config(
+    page_title="BI Logístico",
+    page_icon="🚚",
+    layout="wide"
+)
+
+
+st.title("🚚 BI Logístico — Painel Executivo")
+st.markdown("Análise de Operação Logística")
